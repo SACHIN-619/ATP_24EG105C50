58f09f984abeb3294ebb890cd21447c0ab1c33ce
SACHIN-619 <sachinkr52990@gmail.com>
2026-06-19 23:36:47 +0530

    Modified and kept tailwindcss plugin in config file

--- Individual/Freelance_bid/frontend/vite.config.js
+++ Individual/Freelance_bid/frontend/vite.config.js
@@ -25,10 +25,14 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 🌟 Step 1: Import the Tailwind v4 plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss() // 🌟 Step 2: Add the plugin instance to the array
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true
